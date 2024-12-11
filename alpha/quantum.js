f9e854f8261e470e283438f1a93abef392c12be4
Robert <forward6@users.noreply.github.com>
2024-12-11 13:34:40 -0500

    lil changes

--- alpha/quantum.js
+++ alpha/quantum.js
@@ -58,6 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+<!-- MailerLite Universal -->
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1096863');
+<!-- End MailerLite Universal -->
+
 console.log(`
 Welcome to the source code, fellow reality debugger!
 If you're reading this, you've discovered our quantum dev tools.
